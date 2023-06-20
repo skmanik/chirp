f4ad64862077e9316e429ee8b07f06d90ad908eb
Sunny Manik <sundeepkmanik@gmail.com>
2023-06-19 20:11:12 -0700

    updating schema to have posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,12 @@ import { useUser } from "@clerk/nextjs";
 import { SignOutButton } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   // const { user } = useUser();
   const { isLoaded, isSignedIn, user } = useUser();
+
+  // tRPC lets you make server functions on vercel
+  // fetch datas from database
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -19,6 +22,9 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {isSignedIn && <div><SignOutButton /></div>}
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
       </main>
     </>
   );
